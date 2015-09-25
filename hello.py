c26821733906f2772af7bad1aafa0c53f60e9035
hkmshb <hkmshb@gmail.com>
2015-09-26 00:21:06 +0100

    Exposed a wsgi interface so that project can be run via a WSGI server.

--- hello.py
+++ hello.py
@@ -18,6 +18,7 @@ settings.configure(
 
 
 from django.conf.urls import url
+from django.core.wsgi import get_wsgi_application
 from django.http import HttpResponse
 
 
@@ -29,6 +30,8 @@ urlpatterns = (
     url(r'^$', index),
 )
 
+
+application = get_wsgi_application()
 
 if __name__ == '__main__':
     from django.core.management import execute_from_command_line
