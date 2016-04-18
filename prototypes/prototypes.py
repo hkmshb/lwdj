ad7ef4073ce06ed315620746960c72bfacd6e888
hkmshb <hkmshb@gmail.com>
2016-04-18 12:20:23 +0100

    Added prototype layouts, navigation and a few pages.

--- prototypes/prototypes.py
+++ prototypes/prototypes.py
@@ -4,43 +4,29 @@ import sys
 from django.conf import settings
 
 
+BASE_DIR = os.path.dirname(__file__)
 
 DEBUG = os.environ.get('DEBUG', 'on') == 'on'
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
-SECRET_KEY = os.environ.get('SECRET_KEY', 'o%_(jt*w((r$baw$@4_o_7s64^4j8@b)j6x=&o5r=9hh-v14zs')
+SECRET_KEY = os.environ.get('SECRET_KEY', 
+                'o%_(jt*w((r$baw$@4_o_7s64^4j8@b)j6x=&o5r=9hh-v14zs')
 
 
 settings.configure(
     DEBUG=DEBUG,
     SECRET_KEY=SECRET_KEY,
     ALLOWED_HOSTS=ALLOWED_HOSTS,
-    ROOT_URLCONF=__name__,
-    MIDDLEWARE_CLASSES=(
-        'django.middleware.common.CommonMiddleware',
-        'django.middleware.csrf.CsrfViewMiddleware',
-        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ROOT_URLCONF='sitebuilder.urls',
+    MIDDLEWARE_CLASSES=(),
+    INSTALLED_APPS=(
+        'django.contrib.staticfiles',
+        'sitebuilder',
     ),
+    STATIC_URL='/static/',
+    SITE_PAGES_DIR=os.path.join(BASE_DIR, 'pages')
 )
-
-from django.conf.urls import url
-from django.core.wsgi import get_wsgi_application
-from django.http import HttpResponse
-
-
-
-def index(request):
-    return HttpResponse('Hello World')
-
-
-urlpatterns = (
-    url(r'^$', index),
-)
-
-
-application = get_wsgi_application()
 
 
 if __name__ == '__main__':
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
-
